c5eced051f7a7bb52e59a13825009560f90a9016
Bastean <bastean@pm.me>
2025-08-01 08:52:32 +0000

    refactor(syncenv): move main logic to internal app package

--- tools/cmd/syncenv/syncenv.go
+++ tools/cmd/syncenv/syncenv.go
@@ -1,42 +1,12 @@
 package main
 
 import (
-	"flag"
-	"log"
-
 	"github.com/bastean/x/tools/internal/app/syncenv"
-	"github.com/bastean/x/tools/internal/pkg/cli"
 	"github.com/bastean/x/tools/internal/pkg/errs"
 )
 
-var (
-	template, envs string
-)
-
 func main() {
-	flag.StringVar(&template, "t", "", "Path to \".env\" file template (required)")
-
-	flag.StringVar(&envs, "e", "", "Path to \".env\" files directory (required)")
-
-	flag.Usage = func() {
-		cli.Usage("syncenv")
-	}
-
-	flag.Parse()
-
-	if flag.NFlag() < 2 {
-		flag.Usage()
-
-		println()
-
-		errs.Fatal("Please define required flags")
-	}
-
-	log.Println("Starting synchronization...")
-
-	if err := syncenv.Up(template, envs); err != nil {
+	if err := syncenv.Up(); err != nil {
 		errs.Fatal(err.Error())
 	}
-
-	log.Println("Synchronization completed!")
 }
--- tools/internal/app/syncenv/syncenv.go
+++ tools/internal/app/syncenv/syncenv.go
@@ -2,12 +2,15 @@ package syncenv
 
 import (
 	"errors"
+	"flag"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
 
+	"github.com/bastean/x/tools/internal/pkg/cli"
+	"github.com/bastean/x/tools/internal/pkg/errs"
 	"github.com/bastean/x/tools/pkg/syncenv"
 )
 
@@ -15,16 +18,50 @@ const (
 	EnvFile = ".env"
 )
 
-func Up(templatePath, envsDir string) error {
-	env := new(syncenv.Env)
+var (
+	templateFile, envsDir string
+)
 
-	templateEnvs, err := env.Dump(templatePath)
+func Init() error {
+	flag.StringVar(&templateFile, "t", "", "Path to \".env\" file template (required)")
+
+	flag.StringVar(&envsDir, "e", "", "Path to \".env\" files directory (required)")
+
+	flag.Usage = func() {
+		cli.Usage("syncenv")
+	}
+
+	flag.Parse()
+
+	if flag.NFlag() < 2 {
+		flag.Usage()
+
+		println()
+
+		return errs.ErrRequiredFlags
+	}
+
+	return nil
+}
+
+func Up() error {
+	err := Init()
 
 	if err != nil {
 		return err
 	}
 
-	log.Printf("Template: %q", templatePath)
+	log.Println("Starting...")
+
+	env := new(syncenv.Env)
+
+	templateEnvs, err := env.Dump(templateFile)
+
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Template: %q", templateFile)
 
 	files, err := os.ReadDir(envsDir)
 
@@ -39,7 +76,7 @@ func Up(templatePath, envsDir string) error {
 	for _, file := range files {
 		filename = file.Name()
 
-		if filename == filepath.Base(templatePath) || !strings.Contains(filename, EnvFile) {
+		if filename == filepath.Base(templateFile) || !strings.Contains(filename, EnvFile) {
 			continue
 		}
 
@@ -65,6 +102,8 @@ func Up(templatePath, envsDir string) error {
 
 		log.Printf("Synchronized: %q", filePath)
 	}
+
+	log.Println("Completed!")
 
 	return nil
 }
